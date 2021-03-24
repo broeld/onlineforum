118a98208ad5e9b20af2e0a62c60db46abb88be2
dina <dinaiovcheva@gmail.com>
2021-03-24 13:30:15 +0200

    first lab: fix namespaces

--- DAL.Infrastructure/Repositories/NotificationRepository.cs
+++ DAL.Infrastructure/Repositories/NotificationRepository.cs
@@ -1,10 +1,11 @@
 ﻿using DAL.Domain;
+using DAL.Infrastructure.Repositories.Generic;
 using DAL.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace DAL.Infrastructure.Repositories.Generic
+namespace DAL.Infrastructure.Repositories
 {
     public class NotificationRepository : GenericRepository<Notification>, INotificationRepository
     {
